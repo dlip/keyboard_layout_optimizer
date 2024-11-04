71d09a651175f25cce5364bf84fcd635d6bc4224
Dane Lipscombe <danelipscombe@gmail.com>
2024-11-05 10:30:41 +1100

    cleanup

--- scripts/harite.sh
+++ scripts/harite.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-RUST_LOG=INFO ./target/release/optimize_genetic -l config/keyboard/harite.yml -e config/evaluation/harite.yml -s "djpcsvmroyfetlknagbihwxu"
+RUST_LOG=INFO ./target/release/optimize_genetic -l config/keyboard/harite.yml -e config/evaluation/harite.yml -s "djpcskmroyfenlvtagwihbxu"
