d6fe0820c0ee29020e34676b6662682f62874d1e
Dane Lipscombe <danelipscombe@gmail.com>
2024-11-06 17:37:33 +1100

    fix keys

--- scripts/harite.sh
+++ scripts/harite.sh
@@ -3,7 +3,7 @@ BIN="${1:-optimize_sa}"
 echo $BIN
 cargo build --release --bin $BIN
 
-LAYOUT="bz;qcjkgtyhlevruanmiodfsp,xw.\/'"
+LAYOUT="djkbevrungisacmothylpwxf"
 
 CMD="./target/release/$BIN -l config/keyboard/harite.yml"
 
