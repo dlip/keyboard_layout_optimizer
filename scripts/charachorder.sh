62fb9b16bc332be048b56cc2e55fbaacd885495e
Dane Lipscombe <danelipscombe@gmail.com>
2024-11-16 16:28:18 +1100

    Add charachorder

---
+++ scripts/charachorder.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+BIN="${1:-optimize_sa}"
+echo $BIN
+cargo build --release --bin $BIN
+
+LAYOUT=";'sygrdelahxtvzijw.cfopn,qkbum"
+
+CMD="./target/release/$BIN -l config/keyboard/charachorder.yml"
+
+if [[ "$BIN" == "optimize_sa" || "$BIN" == "optimize_genetic" || "$BIN" == "evaluate" ]]; then
+  CMD+=" -e config/evaluation/charachorder.yml -n ngrams/oxey_english"
+fi
+
+if [[ "$BIN" == "optimize_sa" || "$BIN" == "optimize_genetic" ]]; then
+  CMD+=" -s"
+fi
+
+CMD+=" $LAYOUT"
+export RUST_LOG=INFO
+$CMD
